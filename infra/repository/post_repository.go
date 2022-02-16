b4c39d52ddf833fbfd5311ce4df14f832afa4ff6
daichinagano <d.nagano1002@gmail.com>
2022-02-17 07:59:40 +0900

    保存処理作成（repositopry）

--- infra/repository/post_repository.go
+++ infra/repository/post_repository.go
@@ -19,3 +19,9 @@ func (r *PostRepository) ListPosts() ([]*model.Post, error) {
 	err := r.db.Find(&posts).Error
 	return posts, err
 }
+
+func (r *PostRepository) CreatePost(title string, content string) (*model.Post, error) {
+	post := model.Post{Title: title, Content: content}
+	err := r.db.Create(&post).Error
+	return &post, err
+}
